c517a718a93a7b66ff4ada91e792bb3bae390948
jdestradap <jdestradap@gmail.com>
2016-04-17 19:50:29 -0500

    creating initial skelleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to meetup-clojure-third
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/meetup_clojure_third/core_test.clj
@@ -0,0 +1,7 @@
+(ns meetup-clojure-third.core-test
+  (:require [clojure.test :refer :all]
+            [meetup-clojure-third.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
