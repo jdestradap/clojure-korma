8caca15a39b23c4c27df13400ffdd2e67a65f6a7
jdestradap <jdestradap@gmail.com>
2016-04-17 23:53:29 -0500

    add defentity function

--- src/meetup_clojure_third/entities/core.clj
+++ src/meetup_clojure_third/entities/core.clj
@@ -1,3 +1,6 @@
-(ns meetup-clojure-third.entities.core)
+(ns meetup-clojure-third.entities.core
+  (:refer-clojure :exclude [update])
+  (:require [korma.core :refer :all])
+  (:require [meetup-clojure-third.db.core :refer [db]]))
 
-
+(defentity users)
