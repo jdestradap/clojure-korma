c28a0dc85671a9d6e877bec20848408f6a008143
jdestradap <jdestradap@gmail.com>
2016-04-17 22:10:13 -0500

    add migratus

---
+++ profiles.clj
@@ -0,0 +1,1 @@
+{:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/meetup-clojure-db?user=meetup&password=clojure"}}}
--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [migratus "0.8.13"]
+                 [org.postgresql/postgresql "9.4-1206-jdbc4"]]
   :main ^:skip-aot meetup-clojure-third.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :plugins [[migratus-lein "0.2.6"]]
+  :migratus {:store :database}
+  :profiles
+  {:uberjar {:aot :all}
+   :dev [:profiles/dev]
+   :profiles/dev {}
+   })
